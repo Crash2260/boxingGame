ff81acbd3a40e8a434c7989974f0a240bd78a479
Chris <c.martin2260@gmail.com>
2023-02-05 20:48:39 -0500

    Initial idea for boxer "AI"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class boxer
+{
+    int health;
+    Random rando = new Random();
+
+    public void actionDecider()
+    {
+        if (health > 25)
+        {
+            if (rando.Next(0, 100) >= 50)
+            {
+                boxer.attack();
+            }
+            else
+            {
+                boxer.block();
+            }
+        }
+    }
+
+    public void attack()
+    {
+        int magicNumber = rando.Next(0, 100);
+        if (magicNumber < 40)
+        {
+            boxer.rightHook();
+        }
+        else if (magicNumber >= 40 && magicNumber < 80)
+        {
+            boxer.leftHook();
+        }
+        else
+        {
+            boxer.uppercut();
+        }
+    }
+
+    public void block()
+    {
+        int randoRoll = rando.Next(0, 100)
+        if (randoRoll < 35)
+        {
+            //Hold block for 2 seconds
+            boxer.blockFor(2);
+        }
+        else if (randoRoll >= 35 && randoRoll < 80)
+        {
+            //Hold block for 3 seconds
+            boxer.blockFor(3);
+        }
+        else
+        {
+            //Hold block for 4 seconds
+            boxer.blockFor(4);
+        }
+        
+    }
+
+
+}
